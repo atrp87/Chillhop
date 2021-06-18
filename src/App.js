b819dca72e4652e98fb6f7dbe730506f57925f12
Andrew Peattie <drewpeattie@hotmail.com>
2021-06-18 16:37:37 +0100

    library component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import React, { useState } from 'react';
 import '../src/styles/app.scss';
 import Player from './components/Player';
 import Song from './components/Song';
+import data from './data';
+import Library from './components/Library';
 
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong} 
+      />
+      <Library songs={songs} />
     </div>
   );
 }
